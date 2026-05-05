8c2060ff8d40e394607ad521cc7be87e5fb6d581
anacarchives <anac.archives@gmail.com>
2026-05-05 16:53:38 +0000

    Update layout.js

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -119,6 +119,10 @@ const SIDEBAR_HTML = `
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M23 21v-2a4 4 0 0 0-3-3.87"/><path d="M16 3.13a4 4 0 0 1 0 7.75"/></svg>
                 <span>Utilisateurs</span>
             </a>
+            <a href="modeles.html" class="nav-item" data-page="modeles">
+                <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
+                <span>Modèles</span>
+            </a>
         </div>
 
         <div class="nav-section">
