761dc009f5182bf558a4c1b0e0fd578883e224e8
anacarchives <anac.archives@gmail.com>
2026-05-09 03:06:40 +0000

    Update layout.js

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -95,7 +95,7 @@ const SIDEBAR_HTML = `
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 8v13H3V8"/><path d="M1 3h22v5H1z"/><path d="M10 12h4"/></svg>
                 <span>Archive</span>
             </a>
-            <a href="generator.html" class="nav-item no-directeur" data-page="generator">
+            <a href="generator.html" class="nav-item" data-page="generator">
                 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="12" y1="18" x2="12" y2="12"/><line x1="9" y1="15" x2="15" y2="15"/></svg>
                 <span>Nouvelle saisie</span>
             </a>
